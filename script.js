c31bfa1c6952b72f7bebd9b317ff86a3ff488615
Serra <igor.potard@epita.fr>
2024-09-19 21:17:25 +0200

    feat: try 2

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ const apiToken = "08b3667cfb1873642e48180259ae8df04003facf";
 const apiUrl = "https://alchad-sandbox.pipedrive.com/api/v1/deals?api_token=" + apiToken;
 
 fetch(apiUrl)
-    .then(response => response.json())
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json();
+    })
     .then(data => {
         const deals = data.data;
-        console.log(deals); // Vérifiez les données récupérées
+        console.log('Deals:', deals); // Vérifiez les données récupérées
         processDeals(deals);
     })
     .catch(error => console.error('Erreur:', error));
@@ -29,13 +34,15 @@ function processDeals(deals) {
     const labels = Object.keys(salesData).sort();
     const data = labels.map(date => salesData[date]);
 
-    console.log(labels, data); // Vérifiez les données traitées
+    console.log('Labels:', labels); // Vérifiez les labels
+    console.log('Data:', data); // Vérifiez les données traitées
 
     createChart(labels, data);
 }
 
 function createChart(labels, data) {
     const ctx = document.getElementById('line-chart').getContext('2d');
+    console.log('Canvas context:', ctx); // Vérifiez le contexte du canvas
     const lineChart = new Chart(ctx, {
         type: 'line',
         data: {
