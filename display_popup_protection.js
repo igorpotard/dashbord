b1cf58df67df2e87d3a509934b9f5e96632b9d09
igorpotard <104573542+igorpotard@users.noreply.github.com>
2024-10-03 11:58:18 +0200

    Update display_popup_protection.js

--- display_popup_protection.js
+++ display_popup_protection.js
@@ -111,7 +111,7 @@ if (!window.location.href.match(/#.*$/)) {
                 }
 
                 if (!validateImat()) {
-                  alert('Veuillez remplir l'immatriculation pour permettre d'identifier le vehicule à assurer.');
+                  alert('Veuillez remplir l\'immatriculation pour permettre d'identifier le vehicule à assurer.');
                   return;
                 }
 
