0e926e12541746619f07f7aeec6d96c3ef5b040e
Serra <igor.potard@epita.fr>
2024-10-26 19:38:07 +0200

    test

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import TaskItem from './TaskItem';
 
 function TaskList() {
+	// Liste des tâches
 	const tasks = [
 		{ id: 1, label: 'Travail', importance: 'Haute', description: 'Terminer le rapport' },
 		{ id: 2, label: 'Maison', importance: 'Moyenne', description: 'Nettoyer la cuisine' },
